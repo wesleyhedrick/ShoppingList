4070607f1166b1306b7f7608ec4766034b06a003
GSWes <wesley.hedrick@gmail.com>
2019-07-01 19:43:38 -0400

    added validation and alert to index.js

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
 $(function() {
-    $('form').submit(event=> {
-      event.preventDefault();
-      const entry = $(this).find('input[id="shopping-list-entry"]').val();
-    
+  $('form').submit(event=> {
+    event.preventDefault();
+    const entry = $(this).find('input[id="shopping-list-entry"]').val();
+    if(entry != ''){
       const item = $(`<li><span class="shopping-item">${entry}</span><div class="shopping-item-controls"><button class="shopping-item-toggle button-check"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>`);
       
       $('.shopping-list').append(item);
-    });
-    
-    $('ul').on('click','.shopping-item-toggle',(function(event){
-      event.stopPropagation();
-      const clickedButton = $(event.currentTarget);
-      clickedButton.parent().prev().toggleClass('shopping-item__checked');
-    }));
+      $(this).find('input[id="shopping-list-entry"]').val('');
+    } else {
+      alert('Are you shopping for nothin\'? :)')
+    }
 
-    $('ul').on('click','.shopping-item-delete',(function(event){
-      event.stopPropagation();
-      const clickedButton = $(event.currentTarget);
-      clickedButton.parent().parent().remove();
-    }));
+  });
+  
+  $('ul').on('click','.shopping-item-toggle',(function(event){
+    event.stopPropagation();
+    const clickedButton = $(event.currentTarget);
+    clickedButton.parent().prev().toggleClass('shopping-item__checked');
+  }));
 
+  $('ul').on('click','.shopping-item-delete',(function(event){
+    event.stopPropagation();
+    const clickedButton = $(event.currentTarget);
+    clickedButton.parent().parent().remove();
+  }));
+
+
+  
 });
-    
   
