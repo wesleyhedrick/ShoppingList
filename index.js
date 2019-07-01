0460e147629f659ee3b1737e656d97b60f9da927
GSWes <wesley.hedrick@gmail.com>
2019-07-01 19:21:21 -0400

    created html, js and css files for Shopping List app

---
+++ index.js
@@ -0,0 +1,25 @@
+$(function() {
+    $('form').submit(event=> {
+      event.preventDefault();
+      const entry = $(this).find('input[id="shopping-list-entry"]').val();
+    
+      const item = $(`<li><span class="shopping-item">${entry}</span><div class="shopping-item-controls"><button class="shopping-item-toggle button-check"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>`);
+      
+      $('.shopping-list').append(item);
+    });
+    
+    $('ul').on('click','.shopping-item-toggle',(function(event){
+      event.stopPropagation();
+      const clickedButton = $(event.currentTarget);
+      clickedButton.parent().prev().toggleClass('shopping-item__checked');
+    }));
+
+    $('ul').on('click','.shopping-item-delete',(function(event){
+      event.stopPropagation();
+      const clickedButton = $(event.currentTarget);
+      clickedButton.parent().parent().remove();
+    }));
+
+});
+    
+  
